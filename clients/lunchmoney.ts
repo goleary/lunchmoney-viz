cb378b358cc3a5c6528cdb14f97c57ab92b1215a
Gabe O'Leary <oleary.gabe@gmail.com>
2021-05-24 21:20:46 -0700

    add lunchmoney types & react-table

--- clients/lunchmoney.ts
+++ clients/lunchmoney.ts
@@ -1,5 +1,59 @@
 import axios, { AxiosInstance } from "axios";
 
+// Types
+
+export interface PlaidAccounts {
+  plaid_accounts: PlaidAccount[];
+}
+
+export interface PlaidAccount {
+  id: number;
+  date_linked: Date;
+  name: string;
+  type: string;
+  subtype: null | string;
+  mask: null | string;
+  institution_name: string;
+  status: Status;
+  last_import: Date | null;
+  balance: string;
+  currency: Currency;
+  balance_last_update: Date;
+  limit: number | null;
+}
+
+type Currency = "usd" | string;
+
+export enum Status {
+  Active = "active",
+  NotSupported = "not supported",
+}
+
+export interface ManualAssets {
+  assets: ManualAsset[];
+}
+
+export interface ManualAsset {
+  id: number;
+  type_name: string;
+  subtype_name: null | string;
+  name: string;
+  balance: string;
+  balance_as_of: Date;
+  currency: string;
+  institution_name: null | string;
+  created_at: Date;
+}
+
+type Asset = {
+  name: string;
+  type: string;
+  subtype: null | string;
+  institution_name: string;
+  balance: number;
+  currency: Currency;
+};
+
 export class LunchMoneyClient {
   http: AxiosInstance;
   constructor() {
@@ -13,21 +67,56 @@ export class LunchMoneyClient {
     const response = await this.http.get(
       "https://dev.lunchmoney.app/v1/assets"
     );
-    const { assets } = response.data;
+    const { assets } = response.data as ManualAssets;
     return assets;
   }
   async getPlaidAccounts() {
     const response = await this.http.get(
       "https://dev.lunchmoney.app/v1/plaid_accounts"
     );
-    const { plaid_accounts } = response.data;
+    const { plaid_accounts } = response.data as PlaidAccounts;
     return plaid_accounts;
   }
-  async getAllAssets() {
+  async getAllAssets(): Promise<Asset[]> {
     const manualAssets = await this.getManualAssets();
     const plaidAccounts = await this.getPlaidAccounts();
-    const assets = [...manualAssets, ...plaidAccounts];
+    const assets = [
+      ...manualAssets.map(
+        ({
+          name,
+          type_name,
+          subtype_name,
+          balance,
+          institution_name,
+          currency,
+        }): Asset => ({
+          name,
+          type: type_name,
+          subtype: subtype_name,
+          institution_name,
+          balance: parseFloat(balance),
+          currency,
+        })
+      ),
+      ...plaidAccounts.map(
+        ({
+          name,
+          type,
+          subtype,
+          balance,
+          institution_name,
+          currency,
+        }): Asset => ({
+          name,
+          type,
+          subtype,
+          institution_name,
+          balance: parseFloat(balance),
+          currency,
+        })
+      ),
+    ];
 
-    return assets.filter(({ balance }) => parseFloat(balance) > 0);
+    return assets;
   }
 }
--- pages/assets.tsx
+++ pages/assets.tsx
@@ -1,7 +1,40 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+
+import styled from "styled-components";
+
 import styles from "../styles/Home.module.css";
 import { LunchMoneyClient } from "../clients/lunchmoney";
+import Table from "../components/Table";
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
 
 type AssetsPageProps = {
   assets: any[];
@@ -13,21 +46,66 @@ export const getStaticProps: GetStaticProps<AssetsPageProps> = async () => {
   const assets = await lunchmoney.getAllAssets();
   return { props: { assets } };
 };
+// Create our number formatter.
+const usd = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
 
 const AssetsPage: React.FC<AssetsPageProps> = ({ assets }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Type",
+        columns: [
+          {
+            Header: "Primary",
+            accessor: "type",
+          },
+          {
+            Header: "Secondary",
+            accessor: "subtype",
+          },
+        ],
+      },
+      {
+        Header: "Info",
+        columns: [
+          { Header: "Institution", accessor: "institution_name" },
+          {
+            Header: "Name",
+            accessor: "name",
+          },
+          {
+            Header: "Balance",
+            accessor: "balance",
+            Cell: ({ value }) => (
+              <div style={{ textAlign: "right", fontWeight: 600 }}>
+                {usd.format(value)}
+              </div>
+            ),
+          },
+          {
+            Header: "Status",
+            accessor: "status",
+          },
+        ],
+      },
+    ],
+    []
+  );
   console.log("assets: ", assets);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Assets</h1>
-        <ul>
-          {assets.map((asset) => (
-            <li>
-              {asset.institution_name}: <b>{asset.balance}</b> - {asset.type} -{" "}
-              {asset.subtype}
-            </li>
-          ))}
-        </ul>
+        <Styles>
+          <Table columns={columns} data={assets} />
+        </Styles>
         {/* <p className={styles.description}>
           <code className={styles.code}>{JSON.stringify(assets, null, 2)}</code>
         </p> */}
